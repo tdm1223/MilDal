bdb80a31457de691e8c7a883d063c7d23b288ead
Jegyun Kang <tdm1223@naver.com>
2021-09-06 22:08:17 +0900

    [UPDATE] MainCamera
    - 코드 수정

--- Source/MilDal/MainCamera.cpp
+++ Source/MilDal/MainCamera.cpp
@@ -4,6 +4,15 @@
 AMainCamera::AMainCamera()
 {
     PrimaryActorTick.bCanEverTick = true;
+    CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("MainCamera"));
+    CameraComponent->SetRelativeRotation(FRotator(-40.0f, 0.0f, 0.0f));
+    RootComponent = CameraComponent;
+
+    BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
+    BoxComponent->SetupAttachment(RootComponent);
+    BoxComponent->SetRelativeLocation(FVector(390.0f, 0.0f, -600.0f));
+    BoxComponent->SetRelativeRotation(FRotator(40.0f, 0.0f, 0.0f));
+    BoxComponent->SetBoxExtent(FVector(32.0f, 1280.0f, 640.0f));
 }
 
 void AMainCamera::BeginPlay()
@@ -12,21 +21,14 @@ void AMainCamera::BeginPlay()
     auto PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
     if (PlayerController)
     {
-        UE_LOG(LogTemp, Log, TEXT("Success"));
         PlayerController->SetViewTargetWithBlend(this);
     }
-    else
-    {
-        UE_LOG(LogTemp, Log, TEXT("Fail"));
-    }
-
     CurrentLocation = this->GetActorLocation();
 }
 
 void AMainCamera::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
-    //UE_LOG(LogTemp, Log, TEXT("Tick"));
-    CurrentLocation.X += MoveSpeed * DeltaTime; // use X Y or Z.  Use -= to go the opposite way
+    CurrentLocation.X += MoveSpeed * DeltaTime;
     SetActorLocation(CurrentLocation);
 }
--- Source/MilDal/MainCamera.h
+++ Source/MilDal/MainCamera.h
@@ -1,25 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Camera/CameraActor.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "Camera/CameraComponent.h"
 #include "MainCamera.generated.h"
 
-/**
- *
- */
 UCLASS()
-class MILDAL_API AMainCamera : public ACameraActor
+class MILDAL_API AMainCamera : public AActor
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMainCamera();
 
-public:
-    AMainCamera();
-    virtual void BeginPlay();
-    virtual void Tick(float DeltaTime);
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
 
     // ToDo : 적절한 스피드를 찾은 후에는 내부 변수로 넣어야 한다.
     UPROPERTY(EditAnywhere)
     float MoveSpeed;
+
+    UPROPERTY(VisibleAnywhere)
+    UBoxComponent* BoxComponent;
+
+    UPROPERTY(VisibleAnywhere)
+    UCameraComponent* CameraComponent;
 
 private:
     FVector CurrentLocation;
